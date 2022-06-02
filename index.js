58f8653aa47316c86c328c128889931b52ad6326
nguyenquoccuong <nguyenquoccuong98x@gmail.com>
2022-06-02 16:02:45 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const server = http.createServer(app);
 const { Server } = require("socket.io");
+const server = http.createServer(app);
 const io = new Server(server);
 let counter = 0;
 app.get("/", (req, res) => {
