536983b625f3f9c2a69a9ad62894e6ec562e08aa
nguyenquoccuong <nguyenquoccuong98x@gmail.com>
2022-06-02 15:49:40 +0700

    fix io defind

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { count } = require("console");
 const express = require("express");
 const app = express();
 const http = require("http");
-const { Server } = require("socket.io");
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 const io = new Server(server);
 let counter = 0;
 app.get("/", (req, res) => {
@@ -22,10 +21,6 @@ io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
-  io.emit("some event", {
-    someProperty: "some value",
-    otherProperty: "other value",
-  }); // This will emit the event to all connected sockets
 });
 
 server.listen(3000, () => {
